f0ed97e3abdf7d1a75724244b409a08f0a64d810
chautruong79 <62908290+chautruong79@users.noreply.github.com>
2020-06-09 17:07:31 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ import About from "./Components/About";
 import Resume from "./Components/Resume";
 import Contact from "./Components/Contact";
 import Portfolio from "./Components/Portfolio";
-import Particles from "react-particles-js";
 
 class App extends Component {
   constructor(props) {
@@ -44,60 +43,6 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Particles
-          className="particles"
-          params={{
-            particles: {
-              number: {
-                value: 160,
-                density: {
-                  enable: false,
-                },
-              },
-              size: {
-                value: 3,
-                random: true,
-                anim: {
-                  speed: 4,
-                  size_min: 0.3,
-                },
-              },
-              line_linked: {
-                enable: false,
-              },
-              move: {
-                random: true,
-                speed: 1,
-                direction: "top",
-                out_mode: "out",
-              },
-            },
-            interactivity: {
-              events: {
-                onhover: {
-                  enable: true,
-                  mode: "bubble",
-                },
-                onclick: {
-                  enable: true,
-                  mode: "repulse",
-                },
-              },
-              modes: {
-                bubble: {
-                  distance: 250,
-                  duration: 2,
-                  size: 0,
-                  opacity: 0,
-                },
-                repulse: {
-                  distance: 400,
-                  duration: 4,
-                },
-              },
-            },
-          }}
-        />
         <Header data={this.state.resumeData.main} />
         <About data={this.state.resumeData.main} />
         <Resume data={this.state.resumeData.resume} />
